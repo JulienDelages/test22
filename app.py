f6d2a9062de99ada916e1c7609cc0e31ad823679
JulienDelages <delagesjul@eisti.eu>
2021-09-13 16:25:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/gpt2"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+output = query("Can you please let us know more details about your ")
+
 import streamlit as st
 from transformers import pipeline, set_seed
 
@@ -9,10 +20,6 @@ st.title('Hello World!')
 form = st.form(key='my_form')
 text = form.text_input(label='Enter some text')
 submit_button = form.form_submit_button(label='Submit')
-
-
-
-
 
 if submit_button:
     generation = generator(text, max_length=30, num_return_sequences=5)
